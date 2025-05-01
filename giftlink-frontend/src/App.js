5ba6a422025802938f98504560ef06d246ce6bce
Zoltan Bardossi <info@sunny-studio.eu>
2025-05-02 00:15:34 +0200

    Search page ready

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -6,6 +6,8 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
+import SearchPage from './components/SearchPage/SearchPage';
+import DetailsPage from './components/DetailsPage/DetailsPage';
 
 function App() {
 
@@ -18,6 +20,8 @@ function App() {
         <Route path="/app" element={<MainPage />} />
         <Route path="/app/login" element={<LoginPage />} />
         <Route path="app/register" element={<RegisterPage />} />
+        <Route path="/app/search" element={<SearchPage />} />
+        <Route path="/app/product/:productId" element={<DetailsPage />} />
       </Routes>
     </>
   );
